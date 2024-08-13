f4a7469066045c9c75fd19e1506fa4e65df331a9
Vanessa Geny <vanessa.geny@hotmail.fr>
2024-08-13 16:07:02 +0200

    correct bugs due to scss refactor

--- src/project/Projects.js
+++ src/project/Projects.js
@@ -2,23 +2,25 @@ import { projectList } from "../shared/global";
 import Project from "./Project";
 
 const Projects = () => {
+  const handleScroll = (event) => {
+    console.log(event.eventPhase);
+    window.scrollBy({ top: 0, behavior: "smooth" });
+  };
 
-    const handleScroll = (event) => {
-        console.log(event.eventPhase)
-        window.scrollBy({ top: 0, behavior: 'smooth' })
-    }
+  window.onscroll = handleScroll;
 
-    window.onscroll = handleScroll;
-    
-
-    return (
-        <div style={{scrollBehavior: 'smooth'}}>
-            {projectList.map((item, index) => (
-                <Project key={`project-${index}`} {...item} reversed={index % 2 === 1}></Project>
-            ))}
-            <div></div>
-        </div>
-    )
-}
+  return (
+    <div style={{ scrollBehavior: "smooth", position: "absolute" }}>
+      {projectList.map((item, index) => (
+        <Project
+          key={`project-${index}`}
+          {...item}
+          reversed={index % 2 === 1}
+        ></Project>
+      ))}
+      <div></div>
+    </div>
+  );
+};
 
 export default Projects;
