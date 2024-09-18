7480f838eec3b71aa97c8ae7c94a934e16ce9f76
Vanessa Geny <vanessa.geny@hotmail.fr>
2024-09-18 13:59:09 +0200

    fix navbar responsive by adding burger menu for phones and tablets

--- src/shared/component/nav/Navbar.js
+++ src/shared/component/nav/Navbar.js
@@ -2,20 +2,46 @@ import "../../theme.scss";
 import "./Navbar.scss";
 import NavItem from "./NavItem";
 import { navConf } from "./NavConf";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = React.useState(true);
+  const [showBurger, setShowBurger] = React.useState(false);
+
+  const handleResize = (event) => {
+    const width = event.target.innerWidth;
+    if (width > 900) {
+      setShowNav(true);
+      setShowBurger(false);
+    } else {
+      setShowNav(false);
+      setShowBurger(true);
+    }
+  };
+
+  window.addEventListener("resize", handleResize);
+
   return (
     <nav className="nav">
       <div className="nav__container">
-        <ul className="nav__list">
-          {navConf.map((conf) => {
-            return (
-              <li className="nav__item">
-                <NavItem {...conf}></NavItem>
-              </li>
-            );
-          })}
-        </ul>
+        {showBurger && (
+          <div className="nav__burger" onClick={() => setShowNav(!showNav)}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        )}
+        {showNav && (
+          <ul className="nav__list">
+            {navConf.map((conf) => {
+              return (
+                <li className="nav__item">
+                  <NavItem {...conf}></NavItem>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </nav>
   );
